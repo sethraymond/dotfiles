9362a8eab15e169eb333463ae01ca22f6e1005ad
Seth Raymond <seth.raymond@cognex.com>
2023-09-19 11:58:16 -0400

    Fix nvim tree git highlighting

--- .config/nvim/after/plugin/nvimtree.lua
+++ .config/nvim/after/plugin/nvimtree.lua
@@ -19,7 +19,7 @@ require("nvim-tree").setup({
 		preserve_window_proportions = true,
 	},
 	git = {
-		enable = false,
+		enable = true,
 		ignore = true,
 	},
 	filesystem_watchers = {
@@ -32,7 +32,7 @@ require("nvim-tree").setup({
 	},
 	renderer = {
 		root_folder_label = false,
-		highlight_git = false,
+		highlight_git = true,
 		highlight_opened_files = "none",
 
 		indent_markers = {
@@ -44,7 +44,7 @@ require("nvim-tree").setup({
 				file = true,
 				folder = true,
 				folder_arrow = true,
-				git = false,
+				git = true,
 			},
 
 			glyphs = {
