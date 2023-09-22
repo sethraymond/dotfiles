29c00b0ec76b0b7904d41ebdf042852700d95648
Seth Raymond <seth.raymond@cognex.com>
2023-09-22 15:46:25 -0400

    Add git push binding

--- .config/nvim/after/plugin/fugitive.lua
+++ .config/nvim/after/plugin/fugitive.lua
@@ -1,2 +1,3 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "[g]it [s]tatus" })
 vim.keymap.set("n", "<leader>gl", vim.cmd.Flogsplit, { desc = "[g]it [l]og" })
+vim.keymap.set("n", "<leader>gp", "<cmd> Git push<CR>", { desc = "[g]it [p]ush" })
