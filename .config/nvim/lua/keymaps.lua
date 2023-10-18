a547bd8867394ddb70df516a7791090358813c73
Seth Raymond <seth.raymond@cognex.com>
2023-10-18 13:19:18 -0400

    More tpope plugins, ignore copilot

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,7 +1,6 @@
 vim.g.mapleader = " "
 
 -- Normal mode keymaps
-vim.keymap.set("n", "<CR>", "o<Esc>k", { desc = "Add new line below cursor" })
 vim.keymap.set("n", "<leader>ln", "<cmd> set nu! <CR>", { desc = "Set line numbers" })
 vim.keymap.set("n", "<leader>rn", "<cmd> set rnu! <CR>", { desc = "Set relative line numbers" })
 vim.keymap.set("n", "<Esc>", vim.cmd.noh, { desc = "Clear search highlighting" })
