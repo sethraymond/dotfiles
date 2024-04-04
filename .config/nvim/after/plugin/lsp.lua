00866fcae1c51af056a1ce085bd6b9f6647eca45
Seth Raymond <seth.c.raymond@gmail.com>
2024-04-04 17:20:22 -0400

    Overhaul LSP

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,41 +1,6 @@
 require("neoconf").setup({})
-
-require('mason').setup()
-require('mason-lspconfig').setup({
-	ensure_installed = {
-		-- lua
-		"lua_ls",
-		-- web dev
-		"cssls",
-		"html",
-		-- "tsserver",
-		"volar",
-		--python
-		"pyright",
-		-- C++
-		"clangd",
-		-- Docker
-		"dockerls",
-		-- Rust
-		"rust_analyzer"
-	}
-})
-
 local lspconfig = require('lspconfig')
-require('mason-lspconfig').setup_handlers({
-	function(server_name)
-		local server_config = {}
-		if require("neoconf").get(server_name .. ".disable") then
-			return
-		end
-		if server_name == "volar" then
-			server_config.filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'}
-		end
-		lspconfig[server_name].setup(server_config)
-	end,
-})
 
-local lsp = require('lsp-zero').preset({})
 
 require('lint').linters_by_ft = {
 	python = {'flake8'},
@@ -47,78 +12,141 @@ vim.api.nvim_create_autocmd({ "InsertLeave", "BufWritePost", "BufReadPost" }, {
 	end
 })
 
-lsp.on_attach(function(_, bufnr)
-	-- see :help lsp-zero-keybindings
-	-- to learn the available actions
-	lsp.default_keymaps({ buffer = bufnr })
-end)
-
-lsp.configure('pyright', { })
-
-
--- (Optional) Configure lua language server for neovim
-lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
-
+local lspkind = require('lspkind')
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
-local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<Tab>'] = cmp.mapping(function(fallback)
-		if cmp.visible() then
-			local entry = cmp.get_selected_entry()
-			if not entry then
+cmp.setup {
+	sources = {
+		{ name = "copilot", group_index = 2 },
+		{ name = "nvim_lsp"},
+	},
+	snippet = {
+		expand = function(args)
+			require('luasnip').lsp_expand(args.body)
+		end,
+	},
+	formatting = {
+		format = lspkind.cmp_format({
+		  mode = "symbol",
+
+		  max_width = 50,
+		  symbol_map = { Copilot = "" }
+		})
+	},
+	mapping = {
+		['<Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
 				cmp.select_next_item({cmp_select})
-			else
-				cmp.confirm()
-			end
-		else
-			fallback()
-		end
-	end, {"i", "s"}),
-	['<CR>'] = cmp.mapping({
-		i = function (fallback)
-			if cmp.visible() and cmp.get_active_entry() then
-				cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
 			else
 				fallback()
 			end
-		end,
-		s = cmp.mapping.confirm({ select = true }),
-		c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })
-	}),
-	['<C-Space>'] = cmp.mapping.complete(),
+		end, {"i", "s"}),
+		['<CR>'] = cmp.mapping({
+			i = function (fallback)
+				if cmp.visible() and cmp.get_active_entry() then
+					cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+				else
+					fallback()
+				end
+			end,
+			s = cmp.mapping.confirm({ select = true }),
+			c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })
+		}),
+		['<C-Space>'] = cmp.mapping.complete(),
+	}
+}
+
+require('mason').setup()
+require('mason-lspconfig').setup({
+	ensure_installed = {
+		-- lua
+		"lua_ls",
+		-- web dev
+		"cssls",
+		"html",
+		"tsserver",
+		"volar",
+		--python
+		"pyright",
+		-- C++
+		"clangd",
+		-- Docker
+		"dockerls",
+		-- Rust
+		"rust_analyzer"
+	},
 })
 
-lsp.setup_nvim_cmp({
-	mapping = cmp_mappings
-})
-
-local function tab_merge(t1, t2)
-	for i = 1, #t2 do
-		t1[#t1+1] = t2[i]
+local lsp_defaults = {
+	capabilities = require('cmp_nvim_lsp').default_capabilities(
+		vim.lsp.protocol.make_client_capabilities()
+	),
+	on_attach = function(client, bufnr)
+		vim.api.nvim_exec_autocmds('User', {pattern = 'LspAttached'})
 	end
-	return t1
+}
+
+lspconfig.util.default_config = vim.tbl_deep_extend(
+	"force",
+	lspconfig.util.default_config,
+	lsp_defaults
+)
+
+local sign = function (opts)
+	vim.fn.sign_define(opts.name, {
+		texth1 = opts.name,
+		text = opts.text,
+		numhl = '',
+	})
 end
-lsp.set_sign_icons({
-	error = "",
-	warn = "",
-	hint = "",
-	info = "",
+sign({name = 'DiagnosticSignError', text = ""})
+sign({name = 'DiagnosticSignWarn', text = ""})
+sign({name = 'DiagnosticSignHint', text = ""})
+sign({name = 'DiagnosticSignInfo', text = ""})
+
+local group = vim.api.nvim_create_augroup('user_cmds', {clear = true})
+vim.api.nvim_create_autocmd('User', {
+	pattern = 'LspAttached',
+	group = group,
+	callback = function()
+		local bufmap = function(mode, lhs, rhs, desc)
+			local opts = {buffer = true, remap = false, desc = desc}
+			vim.keymap.set(mode, lhs, rhs, opts)
+		end
+		bufmap("n", "gd", function() vim.lsp.buf.definition() end, "LSP [g]et [d]efinition")
+		bufmap("n", "H", function() vim.lsp.buf.hover() end, "LSP [H]over")
+		bufmap("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, "[v]iew [w]orkspace [s]ymbol")
+		bufmap("n", "<leader>vd", function() vim.diagnostic.open_float() end, "[v]iew [d]iagnostic")
+		bufmap("n", "[d", function() vim.diagnostic.goto_next() end, "Next [d]iagnostic")
+		bufmap("n", "]d", function() vim.diagnostic.goto_prev() end, "Prev [d]iagnostic")
+		bufmap("n", "<leader>vca", function() vim.lsp.buf.code_action() end, "[v]iew [c]ode [a]ction")
+		bufmap("n", "<leader>vrr", function() vim.lsp.buf.references() end, "[v]iew code [r]eferences")
+		bufmap("n", "<leader>vrn", function() vim.lsp.buf.rename() end, "[r]e[n]ame")
+		bufmap("n", "<leader>fm", function() vim.lsp.buf.format { async = true } end, "LSP [f]or[m]at")
+		bufmap("i", "<leader>hs", function() vim.lsp.buf.signature_help() end, "[h]elp [s]ignature")
+	end
 })
 
-lsp.on_attach(function(_, bufnr)
-	local opts = { buffer = bufnr, remap = false }
+local default_handler = function(server)
+	local server_config = {}
+	if require('neoconf').get(server .. ".disable") then
+		return
+	end
+	if server == "volar" then
+		server_config.filetypes = { 'vue', 'typescript', 'javascript' }
+	end
+	lspconfig[server].setup(server_config)
+end
 
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, tab_merge(opts, { desc = "LSP [g]et [d]efinition" }))
-	vim.keymap.set("n", "H", function() vim.lsp.buf.hover() end, tab_merge(opts, { desc = "LSP [H]over" }))
-	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, tab_merge(opts, { desc = "[v]iew [w]orkspace [s]ymbol" }))
-	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, tab_merge(opts, { desc = "[v]iew [d]iagnostic" }))
-	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, tab_merge(opts, { desc = "Next [d]iagnostic" }))
-	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, tab_merge(opts, { desc = "Prev [d]iagnostic" }))
-	vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, tab_merge(opts, { desc = "[v]iew [c]ode [a]ction" }))
-	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, tab_merge(opts, { desc = "[v]iew code [r]eferences" }))
-	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, tab_merge(opts, { desc = "[r]e[n]ame" }))
-	vim.keymap.set("n", "<leader>fm", function() vim.lsp.buf.format { async = true } end, tab_merge(opts, { desc = "LSP [f]or[m]at" }))
-	vim.keymap.set("i", "<leader>hs", function() vim.lsp.buf.signature_help() end, tab_merge(opts, { desc = "[h]elp [s]ignature" }))
-end)
-
-lsp.setup()
+require('mason-lspconfig').setup_handlers({
+	default_handler,
+	['tsserver'] = function()
+		lspconfig.tsserver.setup({
+			settings = {
+				completions = {
+					completeFunctionCalls = true
+				}
+			}
+		})
+	end
+})
