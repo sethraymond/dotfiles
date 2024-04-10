86afe9d335a7a2429a419025bf8b21eb6359aefd
Seth Raymond <seth.c.raymond@gmail.com>
2024-04-10 13:54:51 -0400

    Clean up plugins

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,23 +1,10 @@
 return {
-	{
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
-		dependencies = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},             -- Required
-			{'williamboman/mason.nvim'},           -- Optional
-			{'williamboman/mason-lspconfig.nvim'}, -- Optional
-
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},     -- Required
-			{'hrsh7th/cmp-nvim-lsp'}, -- Required
-			{'L3MON4D3/LuaSnip'},     -- Required
-		}
-	},
-	{
-		'mfussenegger/nvim-lint',
-	},
-	{
-		'onsails/lspkind.nvim'
-	},
+	{'neovim/nvim-lspconfig'},
+	{'williamboman/mason.nvim'},
+	{'williamboman/mason-lspconfig.nvim'},
+	{'hrsh7th/nvim-cmp'},
+	{'hrsh7th/cmp-nvim-lsp'},
+	{'L3MON4D3/LuaSnip'},
+	{'mfussenegger/nvim-lint'},
+	{'onsails/lspkind.nvim'},
 }
