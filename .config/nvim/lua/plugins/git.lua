316c61621f957728fecfd1e6c94ea9ef3a2969a4
Seth Raymond <seth.c.raymond@gmail.com>
2024-07-18 15:54:12 -0400

    Add lazygit configs

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -10,4 +10,20 @@ return {
 		"f-person/git-blame.nvim",
 		lazy = false
 	},
+	{
+		"kdheepak/lazygit.nvim",
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		keys = {
+			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "[l]azy[g]it"}
+		}
+	}
 }
