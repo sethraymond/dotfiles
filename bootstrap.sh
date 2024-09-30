df93bebb95369e07b6a123a94729d45bfbd4417d
Seth Raymond <seth.c.raymond@gmail.com>
2024-09-30 09:22:54 -0400

    Hopefully fix root privs?

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,18 @@
 
 set -e
 
-apt-get install -y git stow
+if [ "$EUID" -ne 0 ]; then
+    echo "Switching to root to install packages..."
+    sudo bash <<EOF
+    apt-get update
+    apt-get install -y git stow
+EOF
+else
+    # already running as root, just install...
+    apt-get update
+    apt-get install -y git stow
+fi
+
 git clone https://github.com/sethraymond/dotfiles.git "$HOME/.dotfiles"
 
 pushd "$HOME/.dotfiles"
