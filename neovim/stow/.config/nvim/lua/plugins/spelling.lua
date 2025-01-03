dc14cc067c9e011d1d27a5417c8aff770081eb49
Seth Raymond <seth.c.raymond@gmail.com>
2025-01-03 09:54:23 -0500

    Fix neovim dictionary bug?

--- neovim/stow/.config/nvim/lua/plugins/spelling.lua
+++ neovim/stow/.config/nvim/lua/plugins/spelling.lua
@@ -2,7 +2,7 @@ return {
     "psliwka/vim-dirtytalk",
     build = ":DirtytalkUpdate",
     config = function()
-        vim.opt.spelllang = { "en", "programming" }
         vim.opt.rtp:append(vim.fn.stdpath('data') .. "/site")
+        vim.opt.spelllang = { "en", "programming" }
     end,
 }
